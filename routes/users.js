b65977298d2b82e837a89db504fde7fc0f704bcd
NikitaT206 <acid3602@yandex.ru>
2021-12-25 14:12:40 +0300

    fix: updateUser Joi Validation

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,8 @@ const { updateUser, getUserInfo } = require('../controllers/users');
 router.get('/users/me', getUserInfo);
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    email: Joi.string().email(),
+    name: Joi.string().required.min(2).max(30),
+    email: Joi.string().required.email(),
   }),
 }), updateUser);
 
