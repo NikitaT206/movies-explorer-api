84f4a10dc94ed5533193f635ff8040a4e8008160
NikitaT206 <acid3602@yandex.ru>
2022-02-09 04:10:08 +0300

    fix: add limiter max points

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,5 +2,5 @@ const rateLimit = require('express-rate-limit');
 
 module.exports.limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 50, // limit each IP to 100 requests per windowMs
+  max: 600, // limit each IP to 100 requests per windowMs
 });
